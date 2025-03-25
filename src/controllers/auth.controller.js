be71b5583a5a421876aae8df22980b23438753ed
HugManh <manh2909@gmail.com>
2025-03-25 21:58:52 +0700

    20250325-feat: add user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -207,6 +207,11 @@ const login = async (req, res) => {
             message: 'Login successful',
             accessToken,
             refreshToken,
+            user: {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
         });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
